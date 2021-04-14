f3b525b1675da36517076f26705ecfcd648e3dc7
ManishPoduval <manishpoduval@hotmail.com>
2021-04-14 19:33:54 +0530

    handlebars intro

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 const express = require("express");
 const app = express();
 
+require('dotenv').config()
+
+console.log( process.env.PASSWORD)
 
 const port = 3000;
 
 // require some data form your data.js file
-let {students, instructors} = require('./data')
+let {students, instructors, getStudents, getTeachers} = require('./data')
 
 // just a simple middleware to show you how it works
 // you will always see that console.log when you visit any page
@@ -18,14 +21,47 @@ app.use((req, res, next) => {
 // letting your middleware know where to find all static files
 app.use(express.static(__dirname + "/public"));
 
+// tell express that your are using hbs as template engine
+app.set('view engine', 'hbs')
+app.set('views', __dirname + '/views')
+
+// Register a partial
+let hbs = require('hbs')
+hbs.registerPartials(__dirname + '/views/partials')
+
+
 // ROUTES DEFINED BELOW
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/views/landing.html')
+    // Using a promise to get data
+     getStudents()
+        .then((myStudents) => {
+            let filteredStudents = myStudents.filter((singleStu) => {
+                return singleStu.city == 'Miami'
+            })
+            res.render('landing.hbs', {name: 'Manish', age: 21, students: filteredStudents, layout: false })
+        })
+        .catch(() => {
+            console.log('Something went wrong')
+        })
 });
 
+app.get('/teachers', (req, res) => {
+      // Using a promise to get data
+    getTeachers()
+        .then((teachers) => {
+            //console.log(teachers)
+            res.render('teachers.hbs', {instructors: teachers })
+        })
+        .catch(() => { 
+            console.log('Error ')
+        })  
+})
 
 // Express setup to listen for all client requests on a certain port
 app.listen(port, () =>
     console.log(`Example app listening at http://localhost:${port}`)
 );
+
+
+
